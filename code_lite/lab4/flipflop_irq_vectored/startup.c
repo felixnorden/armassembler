7196ad922573834e8eadbbdef27e38cc797bbb36
NinjaWazzzabi <amk1997@hotmail.se>
2017-12-04 21:11:42 +0100

    Add working version of assignment 6.6

--- code_lite/lab4/flipflop_irq_vectored/startup.c
+++ code_lite/lab4/flipflop_irq_vectored/startup.c
@@ -11,11 +11,11 @@
 #define EXTI_FTSR 				(*((volatile uint32 *)0x40013C0C))
 #define EXTI_RTSR 				(*((volatile uint32 *)0x40013C08))
 #define EXTI_PR 				(*((volatile uint32 *)0x40013C14))
-#define EXIT3_IRQVEC 			(*((void (**)(void))0x2001C064))
-#define EXIT2_IRQVEC 			(*((void (**)(void))0x2001C060))
-#define EXIT1_IRQVEC 			(*((void (**)(void))0x2001C05C))
-#define EXIT0_IRQVEC 			(*((void (**)(void))0x2001C058))
-#define NVIC_ISER0 	 			(*((volatile uint32 *)0x2001C058))
+#define EXTI3_IRQVEC 			(*((void (**)(void))0x2001C064))
+#define EXTI2_IRQVEC 			(*((void (**)(void))0x2001C060))
+#define EXTI1_IRQVEC 			(*((void (**)(void))0x2001C05C))
+#define EXTI0_IRQVEC 			(*((void (**)(void))0x2001C058))
+#define NVIC_ISER0 	 			(*((volatile uint32 *)0xE000E100))
 #define NVIC_EXTI3_IRQ_BPOS 	(1 << 9)
 #define NVIC_EXTI2_IRQ_BPOS 	(1 << 8)
 #define NVIC_EXTI1_IRQ_BPOS 	(1 << 7)
@@ -25,7 +25,16 @@
 #define EXTI1_IRQ_BPOS 			(1 << 1)
 #define EXTI0_IRQ_BPOS 			(1 << 0)
 
-#define OUTPUT GPIO_E.ODR_LOW
+#define OUTPUT GPIO_D.ODR_LOW
+#define IRQ_PINS GPIO_E.ODR_LOW
+
+#define NVIC        0xE000E100
+#define NVIC_ICPR0  (*((uint32*)    (NVIC + 0x180)))
+
+#define EXTI3_IRQ_BPOS (1<<3)
+#define EXTI2_IRQ_BPOS (1<<2)
+#define EXTI1_IRQ_BPOS (1<<1)
+#define EXTI0_IRQ_BPOS (1<<0)
 
 void startup(void) __attribute__((naked)) __attribute__((section(".start_section")));
 
@@ -40,65 +49,63 @@ void startup(void)
 }
 uint32 counter = 0;
 
-void irq_reset()
-{
-	counter = 0;
-	OUTPUT = 0x0;
-
-	EXTI_PR |= EXTI0_IRQ_BPOS;
-	OUTPUT |= 0x10;
-	OUTPUT &= ~0x10;
-}
-
 void irq_incremet()
 {
+	// Not sure why these three are in this order
+	// Feels like the EXTI line should be last 
+	EXTI_PR = EXTI0_IRQ_BPOS;
+	IRQ_PINS |= (1<<4);
+	IRQ_PINS &= ~(1<<4);
+	
 	counter++;
 	OUTPUT = counter;
+}
 
-	EXTI_PR |= EXTI1_IRQ_BPOS;
-	OUTPUT |= 0x20;
-	OUTPUT &= ~0x20;
+void irq_reset()
+{
+	EXTI_PR = EXTI1_IRQ_BPOS;
+	IRQ_PINS |= (1<<5);
+	IRQ_PINS &= ~(1<<5);
+	
+	counter = 0;
+	OUTPUT = counter;
 }
 
 void irq_periodic()
 {
-	if (OUTPUT != 0)
+	if (OUTPUT != 0x00 && OUTPUT != 0xFF)
 	{
-		OUTPUT = 0xFFFF;
+		OUTPUT = 0x00;
 	}
-
 	OUTPUT = ~OUTPUT;
 	
-	EXTI_PR |= EXTI2_IRQ_BPOS;
-	OUTPUT |= 0x40;
-	OUTPUT &= ~0x40;	
+	EXTI_PR = EXTI2_IRQ_BPOS;	
+	IRQ_PINS |= (1<<6);
+	IRQ_PINS &= ~(1<<6);
 }
 
 void init_app(void)
 {
 	GPIO_D.MODER = 0x00005555;
-	GPIO_E.MODER = 0x00001500;
+	GPIO_E.MODER = 0x00005500;
 
 	SYSCFG_EXTICR1 = 0x0444;
-
+    
+    
 	EXTI_IMR = EXTI0_IRQ_BPOS | EXTI1_IRQ_BPOS | EXTI2_IRQ_BPOS;
-	EXTI_FTSR = EXTI0_IRQ_BPOS | EXTI1_IRQ_BPOS | EXTI2_IRQ_BPOS;
-	EXTI_RTSR = 0;
-
-	NVIC_ISER0 = NVIC_EXTI0_IRQ_BPOS | NVIC_EXTI1_IRQ_BPOS | NVIC_EXTI2_IRQ_BPOS;
-	EXIT0_IRQVEC = irq_incremet;
-	EXIT1_IRQVEC = irq_reset;
-	EXIT2_IRQVEC = irq_periodic;
+	EXTI_RTSR = EXTI0_IRQ_BPOS | EXTI1_IRQ_BPOS | EXTI2_IRQ_BPOS;
+	EXTI_PR = (EXTI0_IRQ_BPOS | EXTI1_IRQ_BPOS | EXTI2_IRQ_BPOS);
+    
+    
+	EXTI0_IRQVEC = irq_incremet;
+	EXTI1_IRQVEC = irq_reset;
+	EXTI2_IRQVEC = irq_periodic;
 	
+	NVIC_ISER0 |= NVIC_EXTI0_IRQ_BPOS | NVIC_EXTI1_IRQ_BPOS | NVIC_EXTI2_IRQ_BPOS;
 }
 
 void main(void)
 {
 	init_app();
-	while (1)
-	{
-		uint32 x = GPIO_E.IDR_LOW;
-		uint32 y = GPIO_E.IDR_HIGH;
-		int a = 2;
-	}
+	while (1){}
 }
